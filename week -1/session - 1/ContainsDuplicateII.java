4397b7963915d869ad33c3e03cd5f344fb96f468
KONETI MAHESH BABU <197787033+Mash2006@users.noreply.github.com>
2026-08-27 09:24:29 +0530

    Add ContainsDuplicateII class to check duplicates

---
+++ week -1/session - 1/ContainsDuplicateII.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> lastIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (lastIndex.containsKey(nums[i])) {
+                if (i - lastIndex.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+
+            lastIndex.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
